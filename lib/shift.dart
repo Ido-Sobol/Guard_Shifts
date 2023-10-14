abb7fdb9501978dd73878b695e1822ee3524935a
ido-sobol <idosobol1005@gmail.com>
2023-10-14 14:01:15 +0300

    For live share.

--- lib/shift.dart
+++ lib/shift.dart
@@ -1,4 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shidts/areas.dart';
+import 'package:shidts/guardPair.dart';
+
 class Shift{
-  String date = '';
+  final String date;
+  final GuardPair pair;
+  final Areas area;
+  Shift(this.date, this.pair, this.area);
+
+  DataRow getData() {
+    return DataRow(cells: [
+                  DataCell(Text('2023-10-14')),
+                  DataCell(Text(Areas.EAST.message)),
+                  DataCell(GuardPair(const ['John', 'Alice'])),
+                  DataCell(GuardPair(const ['Bob', 'Eve'])),
+                ]);
+  }
+  
 
 }
